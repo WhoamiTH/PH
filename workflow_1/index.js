0e9278fdd47094dd804d8d3af34c142cc2a0ab4b
Whoami_TH <taihann1@163.com>
2018-08-14 23:00:29 +1000

    function

--- workflow_1/index.js
+++ workflow_1/index.js
@@ -6,9 +6,15 @@
 //         container.append("<div></div>")
 //     })
 // })
-
-var add_btn = document.getElementById("add");
-add_btn.onclick = function() {
-    const container = $(".down-left-middle");
-    container.append("<div></div>")
+// 
+function add_btn()
+{
+	const container = $(".down-left-middle");
+	container.append("<div></div>")
 }
+
+// var add_btn = document.getElementById("add");
+// add_btn.onclick = function() {
+//     const container = $(".down-left-middle");
+//     container.append("<div></div>")
+// }
